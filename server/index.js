a73c29d864d4a0e511d522a8a614e20e4687fd37
tiger1028 <gentleman.tiger1028@gmail.com>
2022-04-14 03:24:00 +0900

    modified for public

--- server/index.js
+++ server/index.js
@@ -7,11 +7,12 @@ const { backendSetup, databaseSetup } = require("./setup");
 
 const app = express();
 
-databaseSetup(() => {
-    backendSetup(app);
+// databaseSetup(() => {
+//     backendSetup(app);
 
-    app.use(express.static(path.join(__dirname, "public")))
-        .set("views", path.join(__dirname, "views"))
-        .set("view engine", "ejs")
-        .get("/", (req, res) => res.render("pages/index"));
-});
+// });
+
+app.use(express.static(path.join(__dirname, "public")))
+    .set("views", path.join(__dirname, "views"))
+    .set("view engine", "ejs")
+    .get("/", (req, res) => res.render("pages/index"));
