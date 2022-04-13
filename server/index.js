b75c81898057d890100f5ee82245163fb9a20c7e
tiger1028 <gentleman.tiger1028@gmail.com>
2022-04-14 00:04:32 +0900

    modified for small change

--- server/index.js
+++ server/index.js
@@ -7,5 +7,15 @@ const { backendSetup, databaseSetup } = require("./setup");
 const app = express();
 
 databaseSetup(() => {
-    backendSetup(app);
+    // backendSetup(app);
+});
+
+app.listen(8080, () => {
+    console.log("app listening");
+
+    app.get("*", (req, res, next) => {
+        res.status(200).json({
+            message: "hello",
+        });
+    });
 });
