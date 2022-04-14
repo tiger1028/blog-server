b7538ca387b815e06ce3e72e7a2db12a8c655cb7
tiger1028 <gentleman.tiger1028@gmail.com>
2022-04-14 10:23:48 +0900

    modified for app

--- server/index.js
+++ server/index.js
@@ -9,10 +9,22 @@ const app = express();
 
 // databaseSetup(() => {
 //     backendSetup(app);
-
 // });
 
-app.use(express.static(path.join(__dirname, "../public")))
-    .set("views", path.join(__dirname, "../views"))
-    .set("view engine", "ejs")
-    .get("/", (req, res) => res.render("pages/index"));
+// console.log(path.join(__dirname, "../", "public"));
+// app.use(express.static(path.join(__dirname, "../", "public")))
+//     .set("views", path.join(__dirname, "../", "views"))
+//     .set("view engine", "ejs")
+//     .get("/", (req, res) => res.render("pages/index"));
+
+// app.listen(8080, () => {
+//     console.info("SUCCESS");
+// });
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(8080, () => {
+    console.log(`App listening at http://localhost:${8080}`);
+});
